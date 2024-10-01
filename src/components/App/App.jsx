f56580a491b75b11bbc56bbf1e07384b8728a9b9
Darien Sessions <darien.sessions@gmail.com>
2024-10-01 11:17:41 -0400

    styling app and adding header

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,11 @@
+import Header from '../Header/Header.jsx';
+
 export default function App() {
   return (
-    <div>
-      <header>Library App</header>
+    <div className="app">
+      <div className="app__wrapper px-2 max-w-2k bg-black font-bold mx-auto">
+        <Header />
+      </div>
     </div>
   );
 }
