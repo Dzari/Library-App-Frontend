18422a219a45e4551c10ffca4b3a654c7a72ef5f
Darien Sessions <darien.sessions@gmail.com>
2024-10-02 09:46:56 -0400

    update text color

--- src/components/Shelf/Shelf.jsx
+++ src/components/Shelf/Shelf.jsx
@@ -4,7 +4,7 @@ import ItemCard from '../ItemCard/ItemCard';
 export default function Shelf({ data }) {
   return (
     <section className='flex flex-col mb-10'>
-      <div className="shelf__title">{data.shelf}</div>
+      <div className="shelf__title text-white">{data.shelf}</div>
       <div className='flex gap-10'>
         {data.cards.map((card) => {
           return <ItemCard card={card} shelf={data.shelf} />;
