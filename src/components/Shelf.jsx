f2990a3dc8a12cc6bfbfc165319e6d10910c25d3
Darien Sessions <darien.sessions@gmail.com>
2024-10-08 13:02:22 -0400

    add screenWidth context and refactor

--- src/components/Shelf.jsx
+++ src/components/Shelf.jsx
@@ -1,45 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
 import ItemCard from './ItemCard';
+
+import { screenWidthContext } from '../contexts/contexts';
 
 export default function Shelf({ data }) {
   let counter = 0;
+
+  const { screenWidth } = useContext(screenWidthContext);
   return (
     <section className="flex flex-col mb-10 p-4">
-      <div className="shelf__title text-white mb-3">{data.shelf}</div>
+      <div className="shelf__container flex justify-between text-white mb-4">
+        <div className="shelf__title">{data.shelf}</div>
+        <button className="shelf__show-all-button">Show All</button>
+      </div>
       <div className="flex gap-10">
         {data.cards.map((card) => {
           counter++;
-          if (window.innerWidth > 1780 && counter <= 6) {
+          if (screenWidth >= 1640 && counter <= 6) {
             return <ItemCard card={card} rounded={data.rounded} />;
-          } else if (
-            window.innerWidth < 1780 &&
-            window.innerWidth >= 1610 &&
-            counter <= 5
-          ) {
+          } else if (screenWidth >= 1380 && counter <= 5) {
             return <ItemCard card={card} rounded={data.rounded} />;
-          } else if (
-            window.innerWidth < 1610 &&
-            window.innerWidth >= 1270 &&
-            counter <= 4
-          ) {
+          } else if (screenWidth >= 1100 && counter <= 4) {
             return <ItemCard card={card} rounded={data.rounded} />;
-          } else if (
-            window.innerWidth < 1270 &&
-            window.innerWidth >= 740 &&
-            counter <= 3
-          ) {
+          } else if (screenWidth >= 675 && counter <= 3) {
             return <ItemCard card={card} rounded={data.rounded} />;
-          } else if (
-            window.innerWidth < 740 &&
-            window.innerWidth >= 500 &&
-            counter <= 2
-          ) {
+          } else if (screenWidth >= 455 && counter <= 2) {
             return <ItemCard card={card} rounded={data.rounded} />;
-          } else if (
-            window.innerWidth < 500 &&
-            window.innerWidth >= 320 &&
-            counter <= 1
-          ) {
+          } else if (counter <= 1) {
             return <ItemCard card={card} rounded={data.rounded} />;
           }
         })}
