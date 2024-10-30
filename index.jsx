d5e1528cae4bc2d82b6b259869b7bf199809a330
Darien Sessions <darien.sessions@gmail.com>
2024-10-29 20:24:59 -0400

    moving index

--- index.jsx
+++ index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './components/App';
-import './index.css';
+import App from './src/components/App';
+import './src/index.css';
 
 ReactDom.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- src/components/signupModal.jsx
+++ src/components/signupModal.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import ModalWithForm from '../components/ModalWithForm';
+import ModalWithForm from './ModalWithForm';
 
 const SignupModal = ({
   handleCloseModal,
